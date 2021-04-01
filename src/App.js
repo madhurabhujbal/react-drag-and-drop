42dc62ae29152087703b2c7aed2d837ddaee222c
Madhura <madhura.bhujbal@gmail.com>
2021-04-01 15:40:21 +0100

    Import and include components in the main file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react'
+import Board from './components/Board'
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <main className="flexbox">
-        Here will be our boards!
+        <Board id='board-1' className='board'>
+          <Card id='card-1' className='card' draggable='true'>
+            <p>Card 1</p>
+          </Card>
+        </Board>
+
+        <Board id='board-2' className='board'>
+          <Card id='card-2' className='card' draggable='true'>
+            <p>Card 2</p>
+          </Card>
+        </Board>
       </main>
     </div>
   );
